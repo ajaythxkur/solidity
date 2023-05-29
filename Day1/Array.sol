eda1d7952ca82e69ed0ff59bfe14a25928f42b9d
ajaythxkur <ajaythakur9131@gmail.com>
2023-05-29 20:28:50 +0530

    bytes array

--- Day1/Array.sol
+++ Day1/Array.sol
@@ -18,4 +18,23 @@ contract Array{
     function popElement() public {
         dynArr.pop();
     }
+    //byte arrays 1 byte = 8 bits, 1 hexadecimal digit = 4 bits
+    // everything that will be stored in the byte array will be in the form of hexadecimal digits
+    bytes3 public b1;  //3 bytes array = 24 bits
+    bytes2 public b2; //2 bytes array = 16 bits
+    function setter() public{
+        b1='abc';
+        b2='ab';
+    }
+    bytes public arrbyte="abc";
+    
+    function pushByteElement() public{
+        arrbyte.push("d");
+    }
+    function getByteElement(uint i) public view returns(bytes1){
+        return arrbyte[i];
+    }
+    function getByteLen() public view returns(uint){
+        return arrbyte.length;
+    }
 }
